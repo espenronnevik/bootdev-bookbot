1e692210cd30d0570deefb39896dc899b3970854
Espen Rønnevik <espen@digitalsoup.eu>
2024-04-12 23:00:00 +0200

    Create functions for reading and counting words. Print number of words in main()

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-def main():
+def read():
     with open("books/frankenstein.txt") as f:
         contents = f.read()
-        print (contents)
+    return contents
 
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def main():
+    contents = read()
+    numwords = count_words(contents)
+    print(f"The book contains {numwords} words")
 
 main()
